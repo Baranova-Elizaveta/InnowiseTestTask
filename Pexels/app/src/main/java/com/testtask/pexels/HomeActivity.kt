931e08abcd451813a51d3879b7c0247c2a3012a3
Elizaveta Baranova <elizaveta.baranova.31.07@gmail.com>
2024-06-06 20:43:33 +0300

    New version of app

--- Pexels/app/src/main/java/com/testtask/pexels/HomeActivity.kt
+++ Pexels/app/src/main/java/com/testtask/pexels/HomeActivity.kt
@@ -1,8 +1,9 @@
 package com.testtask.pexels
 
+import com.testtask.pexels.HomeFragment
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.view.View
+import android.util.Log
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.LinearLayout
@@ -12,111 +13,81 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.android.volley.Response
-import com.android.volley.toolbox.StringRequest
-import com.android.volley.toolbox.Volley
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.testtask.pexels.adapters.WallpaperAdapter
-import com.testtask.pexels.interfaces.RecyclerViewClickLictener
-import com.testtask.pexels.models.WallpaperModel
-import org.json.JSONException
-import org.json.JSONObject
 
-class HomeActivity : AppCompatActivity(), RecyclerViewClickLictener {
-    private lateinit var adapter: RecyclerView.Adapter<*>
-    private lateinit var wallpaperAdapter: WallpaperAdapter
-    private lateinit var wallpaperModelList: List<WallpaperModel>
-    private lateinit var progressBar: ProgressBar
+class HomeActivity : AppCompatActivity() {
     private lateinit var contentView: LinearLayout
     private lateinit var drawerLayout: DrawerLayout
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var topMostRecyclerView: RecyclerView
     private lateinit var searchEt: EditText
     private lateinit var searchIv: ImageView
+    private lateinit var progressBar: ProgressBar
 
-    private var isScrolling = false
-    private var currentItems = 0
-    private var totalItems = 0
-    private var scrollOutItems = 0
-
-    var pageNumber = 1
-    val url = "https://api.pexels.com/v1/curated?page=$pageNumber&per_page=80"
+    private lateinit var activeFragment: Fragment
+    private lateinit var homeFragment: HomeFragment
+    private lateinit var bookmarksFragment: BookmarksFragment
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        if (savedInstanceState == null) {
-            loadFragment(HomeFragment())
-        }
+        try {
+            homeFragment = HomeFragment()
+            bookmarksFragment = BookmarksFragment()
 
-        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
-        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.home -> {
-                    loadFragment(HomeFragment())
-                    true
-                }
-                R.id.favorites -> {
-                    loadFragment(BookmarksFragment())
-                    true
-                }
-                else -> false
-            }
-        }
-
-        wallpaperModelList = ArrayList()
-        wallpaperAdapter = WallpaperAdapter(this, wallpaperModelList)
-
-        recyclerView = findViewById(R.id.recyclerView)
-        recyclerView.adapter = wallpaperAdapter
-
-
-        val gridLayoutManager = GridLayoutManager(this, 2)
-        recyclerView.layoutManager = gridLayoutManager
-
-        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
-            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
-                super.onScrollStateChanged(recyclerView, newState)
-
-                if (newState == RecyclerView.SCROLL_STATE_DRAGGING) {
-                    isScrolling = true
-                }
+            if (savedInstanceState == null) {
+                supportFragmentManager.beginTransaction()
+                    .add(R.id.fragment_container, homeFragment, "HOME_FRAGMENT")
+                    .commit()
+                activeFragment = homeFragment
             }
 
-            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-                super.onScrolled(recyclerView, dx, dy)
-
-                currentItems = gridLayoutManager.childCount
-                totalItems = gridLayoutManager.itemCount
-                scrollOutItems = gridLayoutManager.findFirstVisibleItemPosition()
-
-                if (isScrolling && (currentItems + scrollOutItems == totalItems)) {
-                    isScrolling = false
-                    fetchWallpaper()
+            val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+            bottomNavigationView.setOnItemSelectedListener { item ->
+                when (item.itemId) {
+                    R.id.home -> {
+                        if (activeFragment != homeFragment) {
+                            supportFragmentManager.beginTransaction()
+                                .hide(activeFragment)
+                                .show(homeFragment)
+                                .commit()
+                            activeFragment = homeFragment
+                        }
+                        true
+                    }
+                    R.id.favorites -> {
+                        if (activeFragment != bookmarksFragment) {
+                            if (!bookmarksFragment.isAdded) {
+                                supportFragmentManager.beginTransaction()
+                                    .hide(activeFragment)
+                                    .add(R.id.fragment_container, bookmarksFragment, "BOOKMARKS_FRAGMENT")
+                                    .commit()
+                            } else {
+                                supportFragmentManager.beginTransaction()
+                                    .hide(activeFragment)
+                                    .show(bookmarksFragment)
+                                    .commit()
+                            }
+                            activeFragment = bookmarksFragment
+                        }
+                        true
+                    }
+                    else -> false
                 }
             }
-        })
 
-        progressBar = findViewById(R.id.progress_bar)
-        progressBar.visibility = View.VISIBLE
+            contentView = findViewById(R.id.content_view)
+            drawerLayout = findViewById(R.id.drawer_layout)
+            searchEt = findViewById(R.id.searchEv)
+            searchIv = findViewById(R.id.search_image)
 
-        contentView = findViewById(R.id.content_view)
-        drawerLayout = findViewById(R.id.drawer_layout)
-        searchEt = findViewById(R.id.searchEv)
-        searchIv = findViewById(R.id.search_image)
-
-        searchIv.setOnClickListener {
-            Toast.makeText(this@HomeActivity, "Search Button Clicked", Toast.LENGTH_SHORT).show()
+            searchIv.setOnClickListener {
+                Toast.makeText(this@HomeActivity, "Search Button Clicked", Toast.LENGTH_SHORT).show()
+            }
+        } catch (e: Exception) {
+            Log.e("HomeActivity", "Error in onCreate", e)
+            Toast.makeText(this, "An error occurred: ${e.message}", Toast.LENGTH_LONG).show()
         }
-    }
-
-    private fun loadFragment(fragment: Fragment) {
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragment_container, fragment).commit()
     }
 
     override fun onBackPressed() {
@@ -126,55 +97,4 @@ class HomeActivity : AppCompatActivity(), RecyclerViewClickLictener {
             super.onBackPressed()
         }
     }
-
-    private fun fetchWallpaper() {
-        val request = object : StringRequest(Method.GET, url,
-            Response.Listener<String> { response ->
-                progressBar.visibility = View.GONE
-
-                try {
-                    val jsonObject = JSONObject(response)
-                    val jsonArray = jsonObject.getJSONArray("photos")
-
-                    for (i in 0 until jsonArray.length()) {
-                        val jsObject = jsonArray.getJSONObject(i)
-                        val id = jsObject.getInt("id")
-                        val photographerName = jsObject.getString("photographer")
-
-                        val objectImage = jsObject.getJSONObject("src")
-                        val originalUrl = objectImage.getString("original")
-                        val mediumUrl = objectImage.getString("medium")
-
-                        val wallpaperModel = WallpaperModel(id, originalUrl, mediumUrl, photographerName)
-                        wallpaperModelList += wallpaperModel
-
-                    }
-
-                    wallpaperAdapter.notifyDataSetChanged()
-                    pageNumber++
-                } catch (e: JSONException) {
-                    e.printStackTrace()
-                }
-            },
-            Response.ErrorListener { error ->
-                progressBar.visibility = View.GONE
-                Toast.makeText(this@HomeActivity, "Failed to fetch wallpapers", Toast.LENGTH_SHORT).show()
-            }) {
-            override fun getHeaders(): Map<String, String> {
-                val params: MutableMap<String, String> = HashMap()
-                params["Authorization"] = "BylC73uP6L7DLisjseaGqtcOq4LMlaWGIq2RGzc6quw7Cr2Op1me8Res"
-                return params
-            }
-        }
-
-        val requestQueue = Volley.newRequestQueue(applicationContext)
-        requestQueue.add(request)
-    }
-
-
-    override fun onItemClick(position: Int) {
-        TODO("Not yet implemented")
-    }
 }
-
-
--- Pexels/app/src/main/java/com/testtask/pexels/HomeFragment.kt
+++ Pexels/app/src/main/java/com/testtask/pexels/HomeFragment.kt
@@ -1,17 +1,127 @@
-package com.testtask.pexels
-
+package com.testtask.pexelsgit commit
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.testtask.pexels.R
+import com.testtask.pexels.adapters.WallpaperAdapter
+import com.testtask.pexels.models.WallpaperModel
+import org.json.JSONException
+import org.json.JSONObject
 
 class HomeFragment : Fragment() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var wallpaperAdapter: WallpaperAdapter
+    private lateinit var wallpaperModelList: MutableList<WallpaperModel>
+    private lateinit var progressBar: ProgressBar
+
+    private var isScrolling = false
+    private var currentItems = 0
+    private var totalItems = 0
+    private var scrollOutItems = 0
+
+    private var pageNumber = 1
+    private val url get() = "https://api.pexels.com/v1/search?query=nature&page=$pageNumber&per_page=80"
+
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        recyclerView = view.findViewById(R.id.recyclerView)
+        progressBar = view.findViewById(R.id.progress_bar)
+
+        wallpaperModelList = ArrayList()
+        wallpaperAdapter = WallpaperAdapter(requireContext(), wallpaperModelList)
+
+        recyclerView.adapter = wallpaperAdapter
+        recyclerView.layoutManager = GridLayoutManager(context, 2)
+
+        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+
+                if (newState == RecyclerView.SCROLL_STATE_DRAGGING) {
+                    isScrolling = true
+                }
+            }
+
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+
+                val gridLayoutManager = recyclerView.layoutManager as GridLayoutManager
+                currentItems = gridLayoutManager.childCount
+                totalItems = gridLayoutManager.itemCount
+                scrollOutItems = gridLayoutManager.findFirstVisibleItemPosition()
+
+                if (isScrolling && (currentItems + scrollOutItems == totalItems)) {
+                    isScrolling = false
+                    pageNumber++
+                    fetchWallpaper()
+                }
+            }
+        })
+
+        fetchWallpaper()
+        return view
+    }
+
+    private fun fetchWallpaper() {
+        progressBar.visibility = View.VISIBLE
+
+        val request = object : StringRequest(
+            Method.GET, url,
+            Response.Listener<String> { response ->
+                progressBar.visibility = View.GONE
+                Log.d("HomeFragment", "Response: $response")
+
+                try {
+                    val jsonObject = JSONObject(response)
+                    val jsonArray = jsonObject.getJSONArray("photos")
+
+                    for (i in 0 until jsonArray.length()) {
+                        val jsObject = jsonArray.getJSONObject(i)
+                        val id = jsObject.getInt("id")
+                        val photographerName = jsObject.getString("photographer")
+
+                        val objectImage = jsObject.getJSONObject("src")
+                        val originalUrl = objectImage.getString("original")
+                        val mediumUrl = objectImage.getString("medium")
+
+                        val wallpaperModel = WallpaperModel(id, originalUrl, mediumUrl, photographerName)
+                        wallpaperModelList.add(wallpaperModel)
+                    }
+
+                    wallpaperAdapter.notifyDataSetChanged()
+                } catch (e: JSONException) {
+                    Log.e("HomeFragment", "JSON Parsing error", e)
+                }
+            },
+            Response.ErrorListener { error ->
+                progressBar.visibility = View.GONE
+                Log.e("HomeFragment", "Volley error", error)
+                Toast.makeText(context, "Failed to fetch wallpapers", Toast.LENGTH_SHORT).show()
+            }) {
+            override fun getHeaders(): Map<String, String> {
+                val params: MutableMap<String, String> = HashMap()
+                params["Authorization"] = "BylC73uP6L7DLisjseaGqtcOq4LMlaWGIq2RGzc6quw7Cr2Op1me8Res"
+                return params
+            }
+        }
+
+        val requestQueue = Volley.newRequestQueue(requireContext())
+        requestQueue.add(request)
     }
 }
